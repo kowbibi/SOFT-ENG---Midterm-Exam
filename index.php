4984df6083b709d1feffc1143f22804fe4b4d6b4
kowbibi <kristianbernard.cortez.cvt@eac.edu.ph>
2024-11-05 19:07:40 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit;
+}
+
+echo "Welcome to the Motorcycle Shop!";
+?>
+
+<a href="logout.php">Logout</a>
