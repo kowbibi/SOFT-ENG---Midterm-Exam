867f22878ed095cfdca7f0e68900a520cfb0bba9
kowbibi <kristianbernard.cortez.cvt@eac.edu.ph>
2024-11-05 19:08:28 +0800

    Update dbConfig.php

--- core/dbConfig.php
+++ core/dbConfig.php
@@ -1,1 +1,14 @@
+<?php
+$host = 'localhost';
+$db = 'motorcycle_shop';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database: " . $e->getMessage());
+}
+?>
 
